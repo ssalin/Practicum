058a15dbf68723b1a2bc0453ceb2ef695acb26d9
jelon94 <li4@pdx.edu>
2016-11-07 21:17:03 -0800

    Added in Wire library

--- Firmware/bludaq_firmware_V0.1/blu_defs.h
+++ Firmware/bludaq_firmware_V0.1/blu_defs.h
@@ -8,6 +8,8 @@
 
  //#define DEBUG_EN   // Comment to disable serial debugging 
  #define SERIAL_EN  // Comment to disable serial com
-#define EEPROM_SIZE= 1024;
+#define EEPROM_SIZE=1024;
+#define EEPROM_FLAG=False;
+#define BluetoothFlag=0;  //Bluetooth flag initialize to off
 
  
--- Firmware/bludaq_firmware_V0.1/bludaq_firmware_V0.1.ino
+++ Firmware/bludaq_firmware_V0.1/bludaq_firmware_V0.1.ino
@@ -6,11 +6,12 @@
  */
 
 #include "blu_defs.h"   // Macros & Prototypes
-
+#include "Wire.h"
 
 // Init:
 void setup() {
 
+    Wire.begin();   
    #ifdef SERIAL_EN
     Serial.begin(9600);
   #endif
@@ -24,14 +25,56 @@ void setup() {
 
 // Main:
 void loop() {
+  
+    int addr = 0;
+    byte value = 0;
 
+    // Check bluetooth flag is connected
+
+    // Write to EEPROM memory
+    writeaddr(addr,value);
+
+    // Read from EEPROM memory
+    byte readval = readaddr(addr);
 
 }
 
+// Write to sensor value to address
+void writeaddr(int address, byte val){
+  
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);
+  Wire.write((int)(address >> 8));   // MSB
+  Wire.write((int)(address & 0xFF)); // LSB
+     
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+// Read from memory address
+byte readaddr(int address){
+  
+  byte rData = 0xFF;
+  
+  Wire.beginTransmission(EEPROM_I2C_ADDRESS);
+  
+  Wire.write((int)(address >> 8));   // MSB
+  Wire.write((int)(address & 0xFF)); // LSB
+  Wire.endTransmission();  
+
+  Wire.requestFrom(EEPROM_I2C_ADDRESS, 1);  
+
+  rData =  Wire.read();
+  return rData;
+}
+
+
+
+
+
 
 // Load from EEPROM
-void load(int address)
-{
+void load(int address){
+  
   if (address < EEPROM_SIZE)
   { 
     Serial.println(EEPROM.read(address));
@@ -42,8 +85,8 @@ void load(int address)
   }
 }
 // Store to EEPROM
-void store(int address, int content)
-{
+void store(int address, int content){
+  
   EEPROM.write(address,content);  
   if (address < EEPROM_SIZE && content != NULL)
   { 
@@ -52,5 +95,5 @@ void store(int address, int content)
   else
   {
     Serial.println("Address is out of range"); 
+  }
 }
-
