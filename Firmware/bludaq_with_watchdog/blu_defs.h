ca82f111a3979679c6e50a3256940172ddda2549
jelon94 <li4@pdx.edu>
2016-11-23 21:31:12 -0800

    Revert "RX functions"
    This reverts commit 5e96978ff3bc8db5ebb591c321004092b8fbfd2d.

--- Firmware/bludaq_with_watchdog/blu_defs.h
+++ Firmware/bludaq_with_watchdog/blu_defs.h
@@ -4,11 +4,11 @@
  * Nov 2016
  * 
  */
- 
- // Required for Type Definitions:
- #include <Arduino.h>
 
-
+//
+//  Libraries and Refrences:
+//
+#include <Arduino.h>  // Arduino Type Definitions
 
 
 //
@@ -21,14 +21,12 @@
 
 
 //
-// Other Definitions:
+// Defined Constants:
 //
 
 #define NUM_RELAY 2           // Number of Relays
 //#define BAUD_RATE 115200      // Better Baud Rate for Serial
 #define BAUD_RATE 9600        // Typical Baud Rate for Serial
-#define MAX_MSG_SIZE 24       // 24 Chars + Terminator
-
 
 
 //
@@ -49,11 +47,12 @@
 // Data Structures:
 //
 
+
 // Auth Data - For validating the host
 typedef struct{
   bool auth_set;    // Authentication Paired
-  int key;          // Auth-key
-  byte poll_freq;   // Polling Frequency (power management)
+  byte key;       // Auth-key
+  byte poll_freq; // Polling Frequency (power management)
 } auth_data;
 
 // Sensor Data - Stores "current" values
@@ -79,8 +78,8 @@ typedef struct {
   byte ls   : 1;   // Light sensor
   byte pir  : 1;   // PIR Motion Sensor
 
-  float setpoint;   // Setpoint 
-  int t_duration;   // Toggle Duration
+  float setpoint_0;   // Setpoint 
+  int t_duration_0;   // Toggle Duration
 } auto_data;
  
 
@@ -89,33 +88,25 @@ typedef struct {
 // EEPROM Settings:
 //
 
-#define EEPROM_SIZE 1024;     // Size of EEPROM in Bytes
-#define AUTH_ADDR 0           // Address of Authentication Structure
-#define AUTO_ADDR 12          // Address of Automation Structure(s)
-#define AUTO_SIZE 7           // Size of Automation Struct, 7 per struct, 14 for both structs
+#define EEPROM_SIZE 1024;      // Size of EEPROM in Bytes
+#define AUTH_ADDR 0            // Address of Authentication Structure
+#define AUTO_ADDR 12           // Address of Automation Structure(s)
+#define AUTO_SIZE 7            // Size of Automation Struct, 7 per struct, 14 for both structs
 #define AUTH_SIZE 11           // Size of Authentication Struct
-
+                               // Size of sensor data is 15
 
 //
 // Serial Communication - Message Stack
 // Message Structure:  <Send Type> = <Body Type> or <Value>
-// Transmit Messages: From Device to Serial Host
-// Recieve Messages: From Serial Host to Device
-// Body Messages: Standard message types (not values) used by host and device
 //
 
-// Message Type Sizes:
-#define MESSAGE_LEN 5    // Size of all Message Types
-#define BODYMSG_LEN 9    // Size of all Messages using a <Body> format (not data)
-#define NUM_RX_MSG 9     // Number of RX Message Types
-#define NUM_TX_MSG 12    // Number of TX Message Types
-#define NUM_BODY_MSG 10    // Number of body message types
 
-// Transmit Message Types: 
+// Send Message Types: 
+
 #define M_BADMSG "UNKO="   // Bad Message = <Incoming Message>
 #define M_HELLO  "HELO="   // Hello = <UUID?>
 #define M_AUTH   "AUTH="   // Authentication Status = <T/F>
-#define M_SLEEP  "SLEP="   // Device will sleep = <Duration>
+#define M_SLEEP  "SLEP="  // Device will sleep = <Duration>
 #define M_ERROR  "EROR="   // Device Error = <Error Message or ID>
 #define M_TEMP   "DTMP"    // Temperature = <Value>
 #define M_HUMID  "DHUM"    // Humidity = <Value>
@@ -125,22 +116,17 @@ typedef struct {
 #define M_RL0    "DRL0"    // Relay 0 State = <T/F> (on / off)
 #define M_RL1    "DRL1"    // Relay 1 State = <T/F> (on / off)
 
-// Array of TX Messages
-const char * tx_msg[] {M_BADMSG, M_HELLO, M_AUTH, M_SLEEP, M_ERROR, M_TEMP, M_HUMID, M_PRESS, M_PHOTO, M_PIR, M_RL0, M_RL1};
 
-// Recieve Message Types: <Recieve Type> = <Body Type> or <Value>
+// Incoming Message Types: <Recieve Type> = <Body Type> or <Value>
 #define RM_AUTOCHAN  "AUTN="   // Automation Channel Select = <Channel>
 #define RM_AUTOFLAG  "AUTF="   // Automation Flag Set = <Byte>
-#define RM_AUTOSET   "AUTS="   // Automation Setpoint = <Float> (4-byte)
+#define RM_AUTOSET   "AUTS="   // Automation Setupoint = <Float> (4-byte)
 #define RM_AUTODUR   "AUTD="   // Automation Duration = <Int> (2-byte)
 #define RM_AUTOCONF  "AUTC="   // Automation Complete = <Channel> (for confirmation)
 #define RM_CONFIGP   "CPOL="   // Configure Polling Frequency = <byte>
 #define RM_DATA      "DATA="   // Perform Data Operation = <Start / Stop>
-#define RM_STATUS    "STAT="   // Request Status  =  <Operation Type>
-#define RM_AUTHKEY   "AUTK="   // Request Auth = <KEY>
+#define RM_STATUS    "STAT="   // Request Status  = <Operation Type>
 
-// Array of RX Messages
-const char * rx_msg[] {RM_AUTOCHAN, RM_AUTOFLAG, RM_AUTOSET, RM_AUTODUR, RM_AUTOCONF, RM_CONFIGP, RM_DATA, RM_STATUS, RM_AUTHKEY};
 
 // Body Types:
 #define M_BAD    "BADC"    // Bad Message
@@ -152,8 +138,4 @@ const char * rx_msg[] {RM_AUTOCHAN, RM_AUTOFLAG, RM_AUTOSET, RM_AUTODUR, RM_AUTO
 #define M_END    "ENDD"    // End Operaton 
 #define M_AUTOS  "AUTS"    // Automation Status
 #define M_DATAS  "DATS"    // Data Status
-#define M_AUTHS  "ATHS"     // Authentication Status
-
-// Array of Body Messages:
-const char * body_msg[] = {M_BAD, M_FALSE, M_TRUE, M_ERROR, M_NOAUTH, M_START, M_END, M_AUTOS, M_DATAS };
 
