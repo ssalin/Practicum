1e91211804b72a685a70ea53c821a01985318aee
shmandy <ae3@pdx.edu>
2016-11-25 02:08:30 -0800

    + Automation UI Work

--- Software/HM_10_iOS/BlueDaq Control/BlueDaq Control/AutoViewController.swift
+++ Software/HM_10_iOS/BlueDaq Control/BlueDaq Control/AutoViewController.swift
@@ -10,23 +10,33 @@ import Foundation
 
 
 
-final class AutoViewController: UIViewController, BluetoothSerialDelegate {
+final class AutoViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewDataSource, BluetoothSerialDelegate {
 
 //MARK: Variables
 
+    var pickerData = [["Temperature", "Pressure", "Humidity", "Light Level"]]
     var message_buffer : String = ""                // Global Message Buffer
     let serial_core = bludaq_core_serial()          // Serial Message Functions
     let accesories = bluedaq_settings()             // App Settings Class
     var current_settings = bluedaq_settings.prefs(init_ : true, auth : false, timestamp : "", last_UUID : "", passcode : "", last_device_name : "", automation_0 : false, automation_1 : false)
-
+    
 
 
 //MARK: IBOutlets
     
+    @IBOutlet weak var title_label: UILabel!
+    @IBOutlet weak var thresh_label: UILabel!
+    @IBOutlet weak var automation_label: UILabel!
+    @IBOutlet weak var motion_label: UILabel!
+    @IBOutlet weak var sensor_selector: UIPickerView!
+    @IBOutlet weak var threshold_txtbox: UITextField!
+    @IBOutlet weak var asc_switch: UISwitch!
+    @IBOutlet weak var motion_switch: UISwitch!
+    @IBOutlet weak var asc_label: UILabel!
+    @IBOutlet weak var chan_select: UISegmentedControl!
     
 
-
-
+    
 //MARK: Functions
     
     override func viewDidLoad() {
@@ -39,14 +49,51 @@ final class AutoViewController: UIViewController, BluetoothSerialDelegate {
             title = "Bluetooth Disabled"
             return
         }
+        
+        // Picker
+        load_picker()
     }
     
     
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
+  
+//MARK: Picker
+
+    // Setup Passcode Picker
+    func load_picker(){
+        
+        // Connect data:
+        self.sensor_selector.delegate = self
+        self.sensor_selector.dataSource = self
+        
+        // Input data into the Array:
+        
+    }
     
+    //# of Components (col)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerData.count
+    }
     
+    // Row Count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData[component].count
+    }
+
+    // Data Deligate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    // Get Selected Value
+    func getDevice() -> String{
+       
+        return pickerData[0][sensor_selector.selectedRow(inComponent: 0)]
+
+    }
+
 
 //MARK: BluetoothSerialDelegate
     
@@ -68,7 +115,34 @@ final class AutoViewController: UIViewController, BluetoothSerialDelegate {
             hud?.hide(true, afterDelay: 1.0)
         }
     }
+
+//MARK: IBActions:
+
+
+    @IBAction func asc_button(_ sender: Any) {
     
+        if asc_switch.isOn == true{ // Above
+            asc_label.text = "above"
+        }
+        else{
+            asc_label.text = "below"
+        }
+    
+    }
+
+
+    @IBAction func motion_button(_ sender: Any) {
+    }
+
+
+    @IBAction func chan_button(_ sender: Any) {
+    }
+
+    @IBAction func txtbox_button(_ sender: Any) {
+    }
+
+
+
 //MARK: Segue Prep
 
 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- Software/HM_10_iOS/BlueDaq Control/BlueDaq Control/InitViewController.swift
+++ Software/HM_10_iOS/BlueDaq Control/BlueDaq Control/InitViewController.swift
@@ -48,6 +48,7 @@ final class InitViewController: UIViewController, UIPickerViewDelegate, UIPicker
         serial.writeType = .withoutResponse
     
         reloadView() // Reload View Functions
+       
     }
     
 
@@ -60,6 +61,9 @@ final class InitViewController: UIViewController, UIPickerViewDelegate, UIPicker
     // Update UI when activated:
     func reloadView() {
 		
+        // Debug:
+         debug_views()
+        
 		// Control Serial Deligate (rx/tx)
 		serial.delegate = self
 		
@@ -86,7 +90,6 @@ final class InitViewController: UIViewController, UIPickerViewDelegate, UIPicker
             connect_button.setTitle("Choose Device", for: .normal)
             connect_button.tintColor = view.tintColor
             connect_button.isEnabled = true
-            //user_authd = false
             current_settings.auth = false
             
         } else {
@@ -96,7 +99,6 @@ final class InitViewController: UIViewController, UIPickerViewDelegate, UIPicker
             connect_button.setTitle("Choose Device", for: .normal)
             connect_button.tintColor = UIColor.gray
             connect_button.isEnabled = true
-            //user_authd = false
             current_settings.auth = false
         }
 		
@@ -110,8 +112,8 @@ final class InitViewController: UIViewController, UIPickerViewDelegate, UIPicker
             enable_views()
         }
         else{
-            gesure_rec.isEnabled = false;
-            pageView.isHidden = true;
+          //  gesure_rec.isEnabled = false;
+          //  pageView.isHidden = true;
         }
         
     }
@@ -250,6 +252,13 @@ final class InitViewController: UIViewController, UIPickerViewDelegate, UIPicker
 	
 	
 //MARK: Enable Applicaton ViewControllers 
+
+    func debug_views(){
+        gesure_rec.isEnabled = true;
+        pageView.isHidden = false;
+    
+    
+    }
 
     func enable_views(){
         passcode_label.text = "Device Connected"
